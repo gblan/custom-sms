8629867c2ab7e469989af57f6a2e2d98914e6d1a
guillaume.blanchard37@gmail.com <GitHubGb2016>
2018-01-10 21:52:09 +0100

    commit SMS

--- app/src/main/java/sms/custom/MainActivity.java
+++ app/src/main/java/sms/custom/MainActivity.java
@@ -1,15 +1,24 @@
 package sms.custom;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_PERMISSION_SEND_SMS =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +35,102 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
         configurerBoutton();
     }
 
-    public void configurerBoutton(){
+    public void configurerBoutton() {
         Button button = (Button) findViewById(R.id.buttonGoPage2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(),Second.class));
+                startActivity(new Intent(getBaseContext(), Second.class));
             }
         });
+
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
+
+    }
+
+    protected void sendSMSMessage() {
+        showPhoneStatePermission();
+
+        //On récupère ce qui a été entré dans les EditText
+        String num = "";
+        String msg = "contenu du message sms envoyé";
+        //Si le numéro est supérieur à 4 caractères et que le message n'est pas vide on lance la procédure d'envoi
+            //Grâce à l'objet de gestion de SMS (SmsManager) que l'on récupère via la méthode static getDefault()
+            //On envoie le SMS à l'aide de la méthode sendTextMessage
+        try {
+
+            SmsManager.getDefault().sendTextMessage(num, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent to : "+num,
+                    Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS to "+num+" faild, please try again later!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void showPhoneStatePermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.SEND_SMS);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.SEND_SMS, REQUEST_PERMISSION_SEND_SMS);
+            } else {
+                requestPermission(Manifest.permission.SEND_SMS, REQUEST_PERMISSION_SEND_SMS);
+            }
+        } else {
+            System.out.println("Permission (already) Granted!");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String permissions[],
+            int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_SEND_SMS:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void showExplanation(String title,
+                                 String message,
+                                 final String permission,
+                                 final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode);
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{permissionName}, permissionRequestCode);
     }
 }
